1aa6ba8ca0a82ba1fbb376b756be359727e02bb2
eom-eom <elbertmolina_25@hotmail.com>
2015-11-24 16:34:51 +0800

    main menu

--- EuroAccounting/UserControl/MainMenu2.Designer.vb
+++ EuroAccounting/UserControl/MainMenu2.Designer.vb
@@ -27,57 +27,121 @@ Partial Class MainMenu2
         Me.btn_Manage_Acc = New System.Windows.Forms.Button()
         Me.btn_Users = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btn_View_Journal
         '
-        Me.btn_View_Journal.Location = New System.Drawing.Point(73, 62)
+        Me.btn_View_Journal.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_View_Journal.FlatAppearance.BorderSize = 0
+        Me.btn_View_Journal.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_View_Journal.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_View_Journal.ForeColor = System.Drawing.Color.Gold
+        Me.btn_View_Journal.Location = New System.Drawing.Point(46, 141)
         Me.btn_View_Journal.Name = "btn_View_Journal"
         Me.btn_View_Journal.Size = New System.Drawing.Size(198, 129)
         Me.btn_View_Journal.TabIndex = 0
         Me.btn_View_Journal.Text = "View Journal"
-        Me.btn_View_Journal.UseVisualStyleBackColor = True
+        Me.btn_View_Journal.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btn_View_Journal.UseVisualStyleBackColor = False
         '
         'btn_Manage_user
         '
-        Me.btn_Manage_user.Location = New System.Drawing.Point(73, 203)
+        Me.btn_Manage_user.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_Manage_user.FlatAppearance.BorderSize = 0
+        Me.btn_Manage_user.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Manage_user.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Manage_user.ForeColor = System.Drawing.Color.Gold
+        Me.btn_Manage_user.Location = New System.Drawing.Point(46, 276)
         Me.btn_Manage_user.Name = "btn_Manage_user"
         Me.btn_Manage_user.Size = New System.Drawing.Size(198, 128)
         Me.btn_Manage_user.TabIndex = 1
         Me.btn_Manage_user.Text = "Manage Users"
-        Me.btn_Manage_user.UseVisualStyleBackColor = True
+        Me.btn_Manage_user.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btn_Manage_user.UseVisualStyleBackColor = False
         '
         'btn_Manage_Acc
         '
-        Me.btn_Manage_Acc.Location = New System.Drawing.Point(277, 62)
+        Me.btn_Manage_Acc.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_Manage_Acc.FlatAppearance.BorderSize = 0
+        Me.btn_Manage_Acc.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Manage_Acc.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Manage_Acc.ForeColor = System.Drawing.Color.Gold
+        Me.btn_Manage_Acc.Location = New System.Drawing.Point(250, 141)
         Me.btn_Manage_Acc.Name = "btn_Manage_Acc"
         Me.btn_Manage_Acc.Size = New System.Drawing.Size(198, 129)
         Me.btn_Manage_Acc.TabIndex = 2
         Me.btn_Manage_Acc.Text = "Manage Accounts"
-        Me.btn_Manage_Acc.UseVisualStyleBackColor = True
+        Me.btn_Manage_Acc.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btn_Manage_Acc.UseVisualStyleBackColor = False
         '
         'btn_Users
         '
-        Me.btn_Users.Location = New System.Drawing.Point(277, 203)
+        Me.btn_Users.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_Users.FlatAppearance.BorderSize = 0
+        Me.btn_Users.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Users.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Users.ForeColor = System.Drawing.Color.Gold
+        Me.btn_Users.Location = New System.Drawing.Point(250, 276)
         Me.btn_Users.Name = "btn_Users"
         Me.btn_Users.Size = New System.Drawing.Size(198, 128)
         Me.btn_Users.TabIndex = 3
         Me.btn_Users.Text = "Users"
-        Me.btn_Users.UseVisualStyleBackColor = True
+        Me.btn_Users.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btn_Users.UseVisualStyleBackColor = False
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(481, 62)
+        Me.Button1.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Gold
+        Me.Button1.Location = New System.Drawing.Point(454, 141)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(198, 129)
         Me.Button1.TabIndex = 4
         Me.Button1.Text = "Tools"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Gold
+        Me.Label1.Location = New System.Drawing.Point(0, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(897, 34)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Welcome! user123"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Gold
+        Me.Button2.Location = New System.Drawing.Point(782, 59)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(115, 34)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Log out"
+        Me.Button2.UseVisualStyleBackColor = False
         '
         'MainMenu2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.btn_Users)
         Me.Controls.Add(Me.btn_Manage_Acc)
@@ -93,5 +157,7 @@ Partial Class MainMenu2
     Friend WithEvents btn_Manage_Acc As System.Windows.Forms.Button
     Friend WithEvents btn_Users As System.Windows.Forms.Button
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
