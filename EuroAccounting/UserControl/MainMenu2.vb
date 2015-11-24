80412a20a1e88df2ccd2d431c15897a6eb0d3dc0
eom-eom <elbertmolina_25@hotmail.com>
2015-11-24 16:40:54 +0800

    main menu again

--- EuroAccounting/UserControl/MainMenu2.vb
+++ EuroAccounting/UserControl/MainMenu2.vb
@@ -11,4 +11,9 @@
     Private Sub btn_Manage_Acc_Click(sender As Object, e As EventArgs) Handles btn_Manage_Acc.Click
 
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+        LogIn.Show()
+    End Sub
 End Class
