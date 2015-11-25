ed5d2229118ddb2759a557fea5a255143f87ba27
sgtsi-jenny <jenny.bercasio@sparkglobaltech.com>
2015-11-25 17:37:25 +0800

    Main menu update

--- EuroAccounting/UserControl/MainMenu2.Designer.vb
+++ EuroAccounting/UserControl/MainMenu2.Designer.vb
@@ -22,6 +22,7 @@ Partial Class MainMenu2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainMenu2))
         Me.btn_View_Journal = New System.Windows.Forms.Button()
         Me.btn_Manage_user = New System.Windows.Forms.Button()
         Me.btn_Manage_Acc = New System.Windows.Forms.Button()
@@ -29,6 +30,7 @@ Partial Class MainMenu2
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btn_View_Journal
@@ -36,11 +38,12 @@ Partial Class MainMenu2
         Me.btn_View_Journal.BackColor = System.Drawing.Color.MidnightBlue
         Me.btn_View_Journal.FlatAppearance.BorderSize = 0
         Me.btn_View_Journal.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btn_View_Journal.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_View_Journal.Font = New System.Drawing.Font("Segoe UI Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_View_Journal.ForeColor = System.Drawing.Color.Gold
-        Me.btn_View_Journal.Location = New System.Drawing.Point(46, 141)
+        Me.btn_View_Journal.Image = CType(resources.GetObject("btn_View_Journal.Image"), System.Drawing.Image)
+        Me.btn_View_Journal.Location = New System.Drawing.Point(54, 141)
         Me.btn_View_Journal.Name = "btn_View_Journal"
-        Me.btn_View_Journal.Size = New System.Drawing.Size(198, 129)
+        Me.btn_View_Journal.Size = New System.Drawing.Size(231, 129)
         Me.btn_View_Journal.TabIndex = 0
         Me.btn_View_Journal.Text = "View Journal"
         Me.btn_View_Journal.TextAlign = System.Drawing.ContentAlignment.BottomCenter
@@ -51,11 +54,12 @@ Partial Class MainMenu2
         Me.btn_Manage_user.BackColor = System.Drawing.Color.MidnightBlue
         Me.btn_Manage_user.FlatAppearance.BorderSize = 0
         Me.btn_Manage_user.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btn_Manage_user.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Manage_user.Font = New System.Drawing.Font("Segoe UI Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Manage_user.ForeColor = System.Drawing.Color.Gold
-        Me.btn_Manage_user.Location = New System.Drawing.Point(46, 276)
+        Me.btn_Manage_user.Image = Global.EuroAccounting.My.Resources.Resources.mngUsers
+        Me.btn_Manage_user.Location = New System.Drawing.Point(54, 276)
         Me.btn_Manage_user.Name = "btn_Manage_user"
-        Me.btn_Manage_user.Size = New System.Drawing.Size(198, 128)
+        Me.btn_Manage_user.Size = New System.Drawing.Size(231, 128)
         Me.btn_Manage_user.TabIndex = 1
         Me.btn_Manage_user.Text = "Manage Users"
         Me.btn_Manage_user.TextAlign = System.Drawing.ContentAlignment.BottomCenter
@@ -66,11 +70,12 @@ Partial Class MainMenu2
         Me.btn_Manage_Acc.BackColor = System.Drawing.Color.MidnightBlue
         Me.btn_Manage_Acc.FlatAppearance.BorderSize = 0
         Me.btn_Manage_Acc.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btn_Manage_Acc.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Manage_Acc.Font = New System.Drawing.Font("Segoe UI Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Manage_Acc.ForeColor = System.Drawing.Color.Gold
-        Me.btn_Manage_Acc.Location = New System.Drawing.Point(250, 141)
+        Me.btn_Manage_Acc.Image = Global.EuroAccounting.My.Resources.Resources.account
+        Me.btn_Manage_Acc.Location = New System.Drawing.Point(292, 141)
         Me.btn_Manage_Acc.Name = "btn_Manage_Acc"
-        Me.btn_Manage_Acc.Size = New System.Drawing.Size(198, 129)
+        Me.btn_Manage_Acc.Size = New System.Drawing.Size(231, 129)
         Me.btn_Manage_Acc.TabIndex = 2
         Me.btn_Manage_Acc.Text = "Manage Accounts"
         Me.btn_Manage_Acc.TextAlign = System.Drawing.ContentAlignment.BottomCenter
@@ -81,11 +86,12 @@ Partial Class MainMenu2
         Me.btn_Users.BackColor = System.Drawing.Color.MidnightBlue
         Me.btn_Users.FlatAppearance.BorderSize = 0
         Me.btn_Users.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btn_Users.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Users.Font = New System.Drawing.Font("Segoe UI Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Users.ForeColor = System.Drawing.Color.Gold
-        Me.btn_Users.Location = New System.Drawing.Point(250, 276)
+        Me.btn_Users.Image = CType(resources.GetObject("btn_Users.Image"), System.Drawing.Image)
+        Me.btn_Users.Location = New System.Drawing.Point(292, 276)
         Me.btn_Users.Name = "btn_Users"
-        Me.btn_Users.Size = New System.Drawing.Size(198, 128)
+        Me.btn_Users.Size = New System.Drawing.Size(231, 128)
         Me.btn_Users.TabIndex = 3
         Me.btn_Users.Text = "Users"
         Me.btn_Users.TextAlign = System.Drawing.ContentAlignment.BottomCenter
@@ -96,11 +102,12 @@ Partial Class MainMenu2
         Me.Button1.BackColor = System.Drawing.Color.MidnightBlue
         Me.Button1.FlatAppearance.BorderSize = 0
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.ForeColor = System.Drawing.Color.Gold
-        Me.Button1.Location = New System.Drawing.Point(454, 141)
+        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
+        Me.Button1.Location = New System.Drawing.Point(530, 141)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(198, 129)
+        Me.Button1.Size = New System.Drawing.Size(231, 129)
         Me.Button1.TabIndex = 4
         Me.Button1.Text = "Tools"
         Me.Button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
@@ -115,9 +122,9 @@ Partial Class MainMenu2
         Me.Label1.ForeColor = System.Drawing.Color.Gold
         Me.Label1.Location = New System.Drawing.Point(0, 59)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(897, 34)
+        Me.Label1.Size = New System.Drawing.Size(1046, 34)
         Me.Label1.TabIndex = 5
-        Me.Label1.Text = "Welcome! user123"
+        Me.Label1.Text = "       Welcome! user123"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'Button2
@@ -126,20 +133,36 @@ Partial Class MainMenu2
         Me.Button2.BackColor = System.Drawing.Color.MidnightBlue
         Me.Button2.FlatAppearance.BorderSize = 0
         Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI Light", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button2.ForeColor = System.Drawing.Color.Gold
-        Me.Button2.Location = New System.Drawing.Point(782, 59)
+        Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
+        Me.Button2.Location = New System.Drawing.Point(917, 59)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(115, 34)
+        Me.Button2.Size = New System.Drawing.Size(44, 34)
         Me.Button2.TabIndex = 6
-        Me.Button2.Text = "Log out"
         Me.Button2.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExit.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btnExit.FlatAppearance.BorderSize = 0
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.Gold
+        Me.btnExit.Image = Global.EuroAccounting.My.Resources.Resources.close1
+        Me.btnExit.Location = New System.Drawing.Point(965, 59)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(37, 34)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.UseVisualStyleBackColor = False
         '
         'MainMenu2
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Silver
+        Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
@@ -147,8 +170,9 @@ Partial Class MainMenu2
         Me.Controls.Add(Me.btn_Manage_Acc)
         Me.Controls.Add(Me.btn_Manage_user)
         Me.Controls.Add(Me.btn_View_Journal)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Name = "MainMenu2"
-        Me.Size = New System.Drawing.Size(897, 469)
+        Me.Size = New System.Drawing.Size(1046, 469)
         Me.ResumeLayout(False)
 
     End Sub
@@ -159,5 +183,6 @@ Partial Class MainMenu2
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
 
 End Class
--- EuroAccounting/UserControl/MainMenu2.vb
+++ EuroAccounting/UserControl/MainMenu2.vb
@@ -16,4 +16,8 @@
         Me.Hide()
         LogIn.Show()
     End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
 End Class
