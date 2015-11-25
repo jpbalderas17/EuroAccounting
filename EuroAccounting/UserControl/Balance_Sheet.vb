f69cfdd2492b4ab91546d837d4ad70d9bb5d71ee
sgtsi-jenny <jenny.bercasio@sparkglobaltech.com>
2015-11-25 17:39:16 +0800

    added balance sheet USC

---
+++ EuroAccounting/UserControl/Balance_Sheet.vb
@@ -0,0 +1,7 @@
+﻿
+Public Class Balance_Sheet
+    Private Sub btn_Cancel_Click(sender As Object, e As EventArgs) Handles btn_Cancel.Click
+        showUSC(uscViewJournal)
+        uscViewJournal.cmbPost.Text = ""
+    End Sub
+End Class
