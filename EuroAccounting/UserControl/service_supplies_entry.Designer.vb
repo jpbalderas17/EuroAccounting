ff47f013734a27aa4f7aee3fb46e61bca3a1d067
sgtsi-jenny <jenny.bercasio@sparkglobaltech.com>
2015-11-26 19:31:54 +0800

    service supply entry new USC

---
+++ EuroAccounting/UserControl/service_supplies_entry.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class service_supplies_entry
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lbl_journal_id = New System.Windows.Forms.Label()
+        Me.txt_amount_paid = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cbo_payment_type = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt_amount_to_be_paid = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.txt_description = New System.Windows.Forms.TextBox()
+        Me.btn_Cancel = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.dt_journal = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbl_journal_type = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.lbl_journal_id)
+        Me.Panel1.Controls.Add(Me.txt_amount_paid)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.cbo_payment_type)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.txt_amount_to_be_paid)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.btnSave)
+        Me.Panel1.Controls.Add(Me.txt_description)
+        Me.Panel1.Controls.Add(Me.btn_Cancel)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.dt_journal)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.lbl_journal_type)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(338, 415)
+        Me.Panel1.TabIndex = 49
+        '
+        'lbl_journal_id
+        '
+        Me.lbl_journal_id.AutoSize = True
+        Me.lbl_journal_id.Font = New System.Drawing.Font("Segoe UI Light", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_journal_id.Location = New System.Drawing.Point(49, 77)
+        Me.lbl_journal_id.Name = "lbl_journal_id"
+        Me.lbl_journal_id.Size = New System.Drawing.Size(72, 17)
+        Me.lbl_journal_id.TabIndex = 61
+        Me.lbl_journal_id.Text = "Journal ID"
+        '
+        'txt_amount_paid
+        '
+        Me.txt_amount_paid.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_amount_paid.Location = New System.Drawing.Point(52, 250)
+        Me.txt_amount_paid.Name = "txt_amount_paid"
+        Me.txt_amount_paid.Size = New System.Drawing.Size(221, 21)
+        Me.txt_amount_paid.TabIndex = 60
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI Light", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(49, 230)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(94, 17)
+        Me.Label4.TabIndex = 59
+        Me.Label4.Text = "Amount paid:"
+        '
+        'cbo_payment_type
+        '
+        Me.cbo_payment_type.FormattingEnabled = True
+        Me.cbo_payment_type.Items.AddRange(New Object() {"Cash", "Downpayment", "On Account"})
+        Me.cbo_payment_type.Location = New System.Drawing.Point(52, 206)
+        Me.cbo_payment_type.Name = "cbo_payment_type"
+        Me.cbo_payment_type.Size = New System.Drawing.Size(221, 21)
+        Me.cbo_payment_type.TabIndex = 58
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI Light", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(49, 186)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(101, 17)
+        Me.Label3.TabIndex = 56
+        Me.Label3.Text = "Payment Type:"
+        '
+        'txt_amount_to_be_paid
+        '
+        Me.txt_amount_to_be_paid.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_amount_to_be_paid.Location = New System.Drawing.Point(52, 162)
+        Me.txt_amount_to_be_paid.Name = "txt_amount_to_be_paid"
+        Me.txt_amount_to_be_paid.Size = New System.Drawing.Size(221, 21)
+        Me.txt_amount_to_be_paid.TabIndex = 55
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Light", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(49, 142)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(133, 17)
+        Me.Label2.TabIndex = 54
+        Me.Label2.Text = "Amount to be paid:"
+        '
+        'btnSave
+        '
+        Me.btnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSave.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btnSave.FlatAppearance.BorderSize = 0
+        Me.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.ForeColor = System.Drawing.Color.Gold
+        Me.btnSave.Location = New System.Drawing.Point(179, 27)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(80, 34)
+        Me.btnSave.TabIndex = 51
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'txt_description
+        '
+        Me.txt_description.Font = New System.Drawing.Font("Segoe UI Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_description.Location = New System.Drawing.Point(52, 304)
+        Me.txt_description.Name = "txt_description"
+        Me.txt_description.Size = New System.Drawing.Size(221, 25)
+        Me.txt_description.TabIndex = 37
+        '
+        'btn_Cancel
+        '
+        Me.btn_Cancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_Cancel.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_Cancel.FlatAppearance.BorderSize = 0
+        Me.btn_Cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Cancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Cancel.ForeColor = System.Drawing.Color.Gold
+        Me.btn_Cancel.Location = New System.Drawing.Point(265, 27)
+        Me.btn_Cancel.Name = "btn_Cancel"
+        Me.btn_Cancel.Size = New System.Drawing.Size(69, 34)
+        Me.btn_Cancel.TabIndex = 50
+        Me.btn_Cancel.Text = "Cancel"
+        Me.btn_Cancel.UseVisualStyleBackColor = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI Light", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(49, 284)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(85, 17)
+        Me.Label7.TabIndex = 36
+        Me.Label7.Text = "Description:"
+        '
+        'dt_journal
+        '
+        Me.dt_journal.Font = New System.Drawing.Font("Segoe UI Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dt_journal.Location = New System.Drawing.Point(52, 114)
+        Me.dt_journal.Name = "dt_journal"
+        Me.dt_journal.Size = New System.Drawing.Size(221, 25)
+        Me.dt_journal.TabIndex = 31
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Light", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(49, 94)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 17)
+        Me.Label1.TabIndex = 30
+        Me.Label1.Text = "Date:"
+        '
+        'lbl_journal_type
+        '
+        Me.lbl_journal_type.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lbl_journal_type.BackColor = System.Drawing.Color.MidnightBlue
+        Me.lbl_journal_type.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_journal_type.ForeColor = System.Drawing.Color.Gold
+        Me.lbl_journal_type.Location = New System.Drawing.Point(0, 27)
+        Me.lbl_journal_type.Name = "lbl_journal_type"
+        Me.lbl_journal_type.Size = New System.Drawing.Size(516, 34)
+        Me.lbl_journal_type.TabIndex = 49
+        Me.lbl_journal_type.Text = "Service"
+        Me.lbl_journal_type.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'service_supplies_entry
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "service_supplies_entry"
+        Me.Size = New System.Drawing.Size(338, 415)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents txt_amount_paid As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents cbo_payment_type As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txt_amount_to_be_paid As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents txt_description As System.Windows.Forms.TextBox
+    Friend WithEvents btn_Cancel As System.Windows.Forms.Button
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents dt_journal As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lbl_journal_type As System.Windows.Forms.Label
+    Friend WithEvents lbl_journal_id As System.Windows.Forms.Label
+
+End Class
---
+++ EuroAccounting/UserControl/service_supplies_entry.vb
@@ -0,0 +1,142 @@
+﻿Public Class service_supplies_entry
+    Dim db As New DBHelper(My.Settings.connectionString)
+    Dim dr As SqlClient.SqlDataReader
+    Dim cmd As SqlClient.SqlCommand
+    Public Function insert_to_journal(journal_data As IDictionary(Of String, Object), journal_entry_data As IDictionary(Of String, Object))
+
+        Dim journal_input As New Dictionary(Of String, Object)
+        Try
+
+
+            '##BEGIN TRANSCTION
+
+            '## INSERT INTO JOURNAL
+            '## GET LAST INSERTED ID of journals
+            journal_input.Add("journal_id", db.ExecuteScalar("INSERT INTO journals(date,description) VALUES(@date,@description);SELECT Scope_Identity()", journal_data).ToString)
+
+
+            '##ORGANIZE input TO BE INSERTED AS JOURNAL ENTRIES
+            '## GET account type 
+            Select Case journal_entry_data("journal_type")
+                Case "Purchase Supplies"
+                    'journal_input.Add("account_type_id", 5)
+                    Select Case journal_entry_data("payment_type")
+                        Case "Cash"
+                            '#DEBIT
+                            journal_input.Add("account_type_id", 5)
+
+                            journal_input.Add("amount", journal_entry_data("to_be_paid"))
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(5,@journal_id,1,'Supplies',@amount)", journal_input)
+
+                            '#CREDIT
+                            journal_input("account_type_id") = 5
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(@account_type_id,@journal_id,0,'Cash',@amount)", journal_input)
+
+                        Case "Downpayment"
+                            '#DEBIT
+                            journal_input.Add("account_type_id", 5)
+
+                            journal_input.Add("amount", journal_entry_data("to_be_paid"))
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(5,@journal_id,1,'Supplies',@amount)", journal_input)
+
+
+
+
+                            '#CREDIT
+                            journal_input("account_type_id") = 6
+                            journal_input("amount") = CDbl(journal_entry_data("to_be_paid")) - CDbl(journal_entry_data("paid"))
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(@account_type_id,@journal_id,0,'Account Payables',@amount)", journal_input)
+
+                            journal_input("account_type_id") = 5
+                            journal_input("amount") = CDbl(journal_entry_data("paid"))
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(@account_type_id,@journal_id,0,'Cash',@amount)", journal_input)
+                        Case "On Account"
+                            '#DEBIT
+                            journal_input.Add("account_type_id", 5)
+
+                            journal_input.Add("amount", journal_entry_data("to_be_paid"))
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(5,@journal_id,1,'Supplies',@amount)", journal_input)
+
+                            '#CREDIT
+                            journal_input("account_type_id") = 6
+                            journal_input("amount") = CDbl(journal_entry_data("to_be_paid")) - CDbl(journal_entry_data("paid"))
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(@account_type_id,@journal_id,0,'Account Payables',@amount)", journal_input)
+                    End Select
+                Case "Payments"
+                    'journal_input.Add("account_type_id", 3)
+                Case "Service"
+
+                    Select Case journal_entry_data("payment_type")
+                        Case "Cash"
+                            '#DEBIT
+                            journal_input.Add("account_type_id", 5)
+
+                            journal_input.Add("amount", journal_entry_data("to_be_paid"))
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(5,@journal_id,1,'Cash',@amount)", journal_input)
+
+                            '#CREDIT
+                            journal_input("account_type_id") = 1
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(@account_type_id,@journal_id,0,'Service Revenue',@amount)", journal_input)
+
+                        Case "Downpayment"
+                            '#DEBIT
+                            journal_input.Add("account_type_id", 5)
+
+                            journal_input.Add("amount", journal_entry_data("paid"))
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(5,@journal_id,1,'Cash',@amount)", journal_input)
+
+                            journal_input("amount") = CDbl(journal_entry_data("to_be_paid")) - CDbl(journal_entry_data("paid"))
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(5,@journal_id,1,'Account Receivables',@amount)", journal_input)
+
+
+                            '#CREDIT
+                            journal_input("account_type_id") = 1
+                            journal_input("amount") = journal_entry_data("to_be_paid")
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(@account_type_id,@journal_id,0,'Service Revenue',@amount)", journal_input)
+                        Case "On Account"
+                            '#DEBIT
+                            journal_input.Add("account_type_id", 5)
+
+                            journal_input.Add("amount", journal_entry_data("to_be_paid"))
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(5,@journal_id,1,'Account Receivables',@amount)", journal_input)
+                            '#CREDIT
+                            journal_input("account_type_id") = 1
+                            db.ExecuteNonQuery("INSERT INTO journal_details(account_type_id,journal_id,is_debit,account_name,amount) VALUES(@account_type_id,@journal_id,0,'Service Revenue',@amount)", journal_input)
+                    End Select
+            End Select
+
+
+
+
+
+
+            '##INSERT JOURNAL DETAILS
+
+
+            '##FINISH TRANSACTION
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        Finally
+            db.Dispose()
+        End Try
+
+        Return False
+
+    End Function
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim journal_data As New Dictionary(Of String, Object)
+        Dim journal_entry_data As New Dictionary(Of String, Object)
+        journal_data.Add("date", dt_journal.Value)
+        journal_data.Add("description", txt_description.Text)
+
+        journal_entry_data.Add("to_be_paid", txt_amount_to_be_paid.Text)
+        journal_entry_data.Add("payment_type", cbo_payment_type.Text)
+        journal_entry_data.Add("paid", txt_amount_paid.Text)
+        journal_entry_data.Add("journal_type", lbl_journal_type.Text)
+        insert_to_journal(journal_data, journal_entry_data)
+    End Sub
+
+    Private Sub btn_Cancel_Click(sender As Object, e As EventArgs) Handles btn_Cancel.Click
+        showUSC(uscMainMenu)
+    End Sub
+End Class
