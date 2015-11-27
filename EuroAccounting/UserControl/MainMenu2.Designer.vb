425ef2561da7e07495c976712724fab38049ed48
eom-eom <elbertmolina_25@hotmail.com>
2015-11-27 09:50:39 +0800

    main menu

--- EuroAccounting/UserControl/MainMenu2.Designer.vb
+++ EuroAccounting/UserControl/MainMenu2.Designer.vb
@@ -30,11 +30,19 @@ Partial Class MainMenu2
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button2 = New System.Windows.Forms.Button()
-        Me.btnExit = New System.Windows.Forms.Button()
         Me.btn_Services = New System.Windows.Forms.Button()
         Me.btn_Purchase = New System.Windows.Forms.Button()
         Me.btn_others = New System.Windows.Forms.Button()
         Me.btn_reports = New System.Windows.Forms.Button()
+        Me.btn_Completion = New System.Windows.Forms.Button()
+        Me.gbx_Choices = New System.Windows.Forms.GroupBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.pnl_mains = New System.Windows.Forms.Panel()
+        Me.btn_cancel = New System.Windows.Forms.Button()
+        Me.btn_sales = New System.Windows.Forms.Button()
+        Me.gbx_Choices.SuspendLayout()
+        Me.pnl_mains.SuspendLayout()
         Me.SuspendLayout()
         '
         'btn_View_Journal
@@ -45,9 +53,9 @@ Partial Class MainMenu2
         Me.btn_View_Journal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_View_Journal.ForeColor = System.Drawing.Color.Gold
         Me.btn_View_Journal.Image = CType(resources.GetObject("btn_View_Journal.Image"), System.Drawing.Image)
-        Me.btn_View_Journal.Location = New System.Drawing.Point(529, 275)
+        Me.btn_View_Journal.Location = New System.Drawing.Point(625, 246)
         Me.btn_View_Journal.Name = "btn_View_Journal"
-        Me.btn_View_Journal.Size = New System.Drawing.Size(231, 129)
+        Me.btn_View_Journal.Size = New System.Drawing.Size(113, 129)
         Me.btn_View_Journal.TabIndex = 0
         Me.btn_View_Journal.Text = "View Journal"
         Me.btn_View_Journal.TextAlign = System.Drawing.ContentAlignment.BottomCenter
@@ -60,7 +68,7 @@ Partial Class MainMenu2
         Me.btn_payments.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btn_payments.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_payments.ForeColor = System.Drawing.Color.Gold
-        Me.btn_payments.Location = New System.Drawing.Point(292, 142)
+        Me.btn_payments.Location = New System.Drawing.Point(270, 113)
         Me.btn_payments.Name = "btn_payments"
         Me.btn_payments.Size = New System.Drawing.Size(231, 128)
         Me.btn_payments.TabIndex = 1
@@ -76,7 +84,7 @@ Partial Class MainMenu2
         Me.btn_Manage_Acc.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Manage_Acc.ForeColor = System.Drawing.Color.Gold
         Me.btn_Manage_Acc.Image = Global.EuroAccounting.My.Resources.Resources.account
-        Me.btn_Manage_Acc.Location = New System.Drawing.Point(529, 410)
+        Me.btn_Manage_Acc.Location = New System.Drawing.Point(507, 381)
         Me.btn_Manage_Acc.Name = "btn_Manage_Acc"
         Me.btn_Manage_Acc.Size = New System.Drawing.Size(231, 129)
         Me.btn_Manage_Acc.TabIndex = 2
@@ -92,7 +100,7 @@ Partial Class MainMenu2
         Me.btn_Users.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Users.ForeColor = System.Drawing.Color.Gold
         Me.btn_Users.Image = CType(resources.GetObject("btn_Users.Image"), System.Drawing.Image)
-        Me.btn_Users.Location = New System.Drawing.Point(529, 142)
+        Me.btn_Users.Location = New System.Drawing.Point(507, 113)
         Me.btn_Users.Name = "btn_Users"
         Me.btn_Users.Size = New System.Drawing.Size(112, 128)
         Me.btn_Users.TabIndex = 3
@@ -108,7 +116,7 @@ Partial Class MainMenu2
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.ForeColor = System.Drawing.Color.Gold
         Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
-        Me.Button1.Location = New System.Drawing.Point(647, 142)
+        Me.Button1.Location = New System.Drawing.Point(625, 113)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(113, 128)
         Me.Button1.TabIndex = 4
@@ -123,9 +131,9 @@ Partial Class MainMenu2
         Me.Label1.BackColor = System.Drawing.Color.MidnightBlue
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Gold
-        Me.Label1.Location = New System.Drawing.Point(0, 59)
+        Me.Label1.Location = New System.Drawing.Point(0, 30)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(1114, 34)
+        Me.Label1.Size = New System.Drawing.Size(875, 34)
         Me.Label1.TabIndex = 5
         Me.Label1.Text = "       Welcome! user123"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -139,25 +147,11 @@ Partial Class MainMenu2
         Me.Button2.Font = New System.Drawing.Font("Segoe UI Light", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button2.ForeColor = System.Drawing.Color.Gold
         Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
-        Me.Button2.Location = New System.Drawing.Point(985, 59)
+        Me.Button2.Location = New System.Drawing.Point(727, 30)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(44, 34)
         Me.Button2.TabIndex = 6
         Me.Button2.UseVisualStyleBackColor = False
-        '
-        'btnExit
-        '
-        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btnExit.BackColor = System.Drawing.Color.MidnightBlue
-        Me.btnExit.FlatAppearance.BorderSize = 0
-        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnExit.ForeColor = System.Drawing.Color.Gold
-        Me.btnExit.Location = New System.Drawing.Point(1033, 59)
-        Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(37, 34)
-        Me.btnExit.TabIndex = 7
-        Me.btnExit.UseVisualStyleBackColor = False
         '
         'btn_Services
         '
@@ -166,7 +160,7 @@ Partial Class MainMenu2
         Me.btn_Services.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btn_Services.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Services.ForeColor = System.Drawing.Color.Gold
-        Me.btn_Services.Location = New System.Drawing.Point(55, 276)
+        Me.btn_Services.Location = New System.Drawing.Point(33, 248)
         Me.btn_Services.Name = "btn_Services"
         Me.btn_Services.Size = New System.Drawing.Size(231, 128)
         Me.btn_Services.TabIndex = 8
@@ -181,7 +175,7 @@ Partial Class MainMenu2
         Me.btn_Purchase.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btn_Purchase.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Purchase.ForeColor = System.Drawing.Color.Gold
-        Me.btn_Purchase.Location = New System.Drawing.Point(55, 142)
+        Me.btn_Purchase.Location = New System.Drawing.Point(270, 246)
         Me.btn_Purchase.Name = "btn_Purchase"
         Me.btn_Purchase.Size = New System.Drawing.Size(231, 128)
         Me.btn_Purchase.TabIndex = 9
@@ -196,7 +190,7 @@ Partial Class MainMenu2
         Me.btn_others.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btn_others.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_others.ForeColor = System.Drawing.Color.Gold
-        Me.btn_others.Location = New System.Drawing.Point(292, 276)
+        Me.btn_others.Location = New System.Drawing.Point(270, 382)
         Me.btn_others.Name = "btn_others"
         Me.btn_others.Size = New System.Drawing.Size(231, 128)
         Me.btn_others.TabIndex = 10
@@ -211,7 +205,7 @@ Partial Class MainMenu2
         Me.btn_reports.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btn_reports.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_reports.ForeColor = System.Drawing.Color.Gold
-        Me.btn_reports.Location = New System.Drawing.Point(55, 411)
+        Me.btn_reports.Location = New System.Drawing.Point(33, 382)
         Me.btn_reports.Name = "btn_reports"
         Me.btn_reports.Size = New System.Drawing.Size(231, 128)
         Me.btn_reports.TabIndex = 11
@@ -219,26 +213,114 @@ Partial Class MainMenu2
         Me.btn_reports.TextAlign = System.Drawing.ContentAlignment.BottomCenter
         Me.btn_reports.UseVisualStyleBackColor = False
         '
+        'btn_Completion
+        '
+        Me.btn_Completion.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_Completion.FlatAppearance.BorderSize = 0
+        Me.btn_Completion.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Completion.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Completion.ForeColor = System.Drawing.Color.Gold
+        Me.btn_Completion.Location = New System.Drawing.Point(507, 247)
+        Me.btn_Completion.Name = "btn_Completion"
+        Me.btn_Completion.Size = New System.Drawing.Size(112, 128)
+        Me.btn_Completion.TabIndex = 12
+        Me.btn_Completion.Text = "Completion of Payments"
+        Me.btn_Completion.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btn_Completion.UseVisualStyleBackColor = False
+        '
+        'gbx_Choices
+        '
+        Me.gbx_Choices.Controls.Add(Me.btn_cancel)
+        Me.gbx_Choices.Controls.Add(Me.Label2)
+        Me.gbx_Choices.Controls.Add(Me.ComboBox1)
+        Me.gbx_Choices.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gbx_Choices.Location = New System.Drawing.Point(250, 178)
+        Me.gbx_Choices.Name = "gbx_Choices"
+        Me.gbx_Choices.Size = New System.Drawing.Size(326, 105)
+        Me.gbx_Choices.TabIndex = 13
+        Me.gbx_Choices.TabStop = False
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(58, 42)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(251, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Select"
+        '
+        'pnl_mains
+        '
+        Me.pnl_mains.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pnl_mains.Controls.Add(Me.btn_sales)
+        Me.pnl_mains.Controls.Add(Me.Label1)
+        Me.pnl_mains.Controls.Add(Me.btn_Completion)
+        Me.pnl_mains.Controls.Add(Me.btn_View_Journal)
+        Me.pnl_mains.Controls.Add(Me.btn_reports)
+        Me.pnl_mains.Controls.Add(Me.btn_payments)
+        Me.pnl_mains.Controls.Add(Me.btn_others)
+        Me.pnl_mains.Controls.Add(Me.btn_Manage_Acc)
+        Me.pnl_mains.Controls.Add(Me.btn_Purchase)
+        Me.pnl_mains.Controls.Add(Me.btn_Users)
+        Me.pnl_mains.Controls.Add(Me.btn_Services)
+        Me.pnl_mains.Controls.Add(Me.Button1)
+        Me.pnl_mains.Controls.Add(Me.Button2)
+        Me.pnl_mains.Location = New System.Drawing.Point(0, 14)
+        Me.pnl_mains.Name = "pnl_mains"
+        Me.pnl_mains.Size = New System.Drawing.Size(875, 529)
+        Me.pnl_mains.TabIndex = 14
+        '
+        'btn_cancel
+        '
+        Me.btn_cancel.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_cancel.FlatAppearance.BorderSize = 0
+        Me.btn_cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_cancel.ForeColor = System.Drawing.Color.Gold
+        Me.btn_cancel.Location = New System.Drawing.Point(239, 69)
+        Me.btn_cancel.Name = "btn_cancel"
+        Me.btn_cancel.Size = New System.Drawing.Size(70, 25)
+        Me.btn_cancel.TabIndex = 2
+        Me.btn_cancel.Text = "Cancel"
+        Me.btn_cancel.UseVisualStyleBackColor = False
+        '
+        'btn_sales
+        '
+        Me.btn_sales.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_sales.FlatAppearance.BorderSize = 0
+        Me.btn_sales.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_sales.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_sales.ForeColor = System.Drawing.Color.Gold
+        Me.btn_sales.Location = New System.Drawing.Point(33, 114)
+        Me.btn_sales.Name = "btn_sales"
+        Me.btn_sales.Size = New System.Drawing.Size(231, 128)
+        Me.btn_sales.TabIndex = 13
+        Me.btn_sales.Text = "Sales"
+        Me.btn_sales.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btn_sales.UseVisualStyleBackColor = False
+        '
         'MainMenu2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Silver
-        Me.Controls.Add(Me.btn_reports)
-        Me.Controls.Add(Me.btn_others)
-        Me.Controls.Add(Me.btn_Purchase)
-        Me.Controls.Add(Me.btn_Services)
-        Me.Controls.Add(Me.btnExit)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.btn_Users)
-        Me.Controls.Add(Me.btn_Manage_Acc)
-        Me.Controls.Add(Me.btn_payments)
-        Me.Controls.Add(Me.btn_View_Journal)
+        Me.Controls.Add(Me.pnl_mains)
+        Me.Controls.Add(Me.gbx_Choices)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Name = "MainMenu2"
-        Me.Size = New System.Drawing.Size(1114, 654)
+        Me.Size = New System.Drawing.Size(875, 555)
+        Me.gbx_Choices.ResumeLayout(False)
+        Me.gbx_Choices.PerformLayout()
+        Me.pnl_mains.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -249,10 +331,16 @@ Partial Class MainMenu2
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents btnExit As System.Windows.Forms.Button
     Friend WithEvents btn_Services As System.Windows.Forms.Button
     Friend WithEvents btn_Purchase As System.Windows.Forms.Button
     Friend WithEvents btn_others As System.Windows.Forms.Button
     Friend WithEvents btn_reports As System.Windows.Forms.Button
+    Friend WithEvents btn_Completion As System.Windows.Forms.Button
+    Friend WithEvents gbx_Choices As System.Windows.Forms.GroupBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents pnl_mains As System.Windows.Forms.Panel
+    Friend WithEvents btn_cancel As System.Windows.Forms.Button
+    Friend WithEvents btn_sales As System.Windows.Forms.Button
 
 End Class
--- EuroAccounting/UserControl/MainMenu2.vb
+++ EuroAccounting/UserControl/MainMenu2.vb
@@ -1,5 +1,7 @@
 ﻿Public Class MainMenu2
+    Private Sub showChoices(mode As Boolean)
 
+    End Sub
     Private Sub btn_View_Journal_Click(sender As Object, e As EventArgs) Handles btn_View_Journal.Click
         showUSC(uscViewJournal)
     End Sub
@@ -18,11 +20,36 @@
         MainMenu.Hide()
     End Sub
 
-    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+    Private Sub btnExit_Click(sender As Object, e As EventArgs)
         Application.Exit()
     End Sub
 
-    Private Sub btn_Manage_user_Click(sender As Object, e As EventArgs) Handles btn_payments.Click
+  
 
+    Private Sub btn_Services_Click(sender As Object, e As EventArgs) Handles btn_Services.Click
+        uscServiceSupplyEntry.lbl_journal_type.Text = "Service"
+        showUSC(uscServiceSupplyEntry)
+    End Sub
+
+    Private Sub btn_Purchase_Click(sender As Object, e As EventArgs) Handles btn_Purchase.Click
+        uscServiceSupplyEntry.lbl_journal_type.Text = "Purchase Supplies"
+        showUSC(uscServiceSupplyEntry)
+    End Sub
+
+    Private Sub btn_payments_Click(sender As Object, e As EventArgs) Handles btn_payments.Click
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles pnl_mains.Paint
+
+    End Sub
+
+    Private Sub btn_Completion_Click(sender As Object, e As EventArgs) Handles btn_Completion.Click
+
+    End Sub
+
+    Private Sub btn_sales_Click(sender As Object, e As EventArgs) Handles btn_sales.Click
+        uscServiceSupplyEntry.lbl_journal_type.Text = "Sales"
+        showUSC(uscServiceSupplyEntry)
     End Sub
 End Class
