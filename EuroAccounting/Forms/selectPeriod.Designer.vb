f04563edc614369d97b19c0c21d46e8c7c1a8b47
sgtsi-jenny <jenny.bercasio@sparkglobaltech.com>
2015-12-04 17:31:00 +0800

    selectPeriod form

---
+++ EuroAccounting/Forms/selectPeriod.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class selectPeriod
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.dtpEndDate = New System.Windows.Forms.DateTimePicker()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.dtpStartDate = New System.Windows.Forms.DateTimePicker()
+        Me.cmbDate = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnView = New System.Windows.Forms.Button()
+        Me.btn_Cancel = New System.Windows.Forms.Button()
+        Me.lblview = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.dtpEndDate)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.dtpStartDate)
+        Me.GroupBox1.Controls.Add(Me.cmbDate)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(22, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(337, 162)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Select Period"
+        '
+        'dtpEndDate
+        '
+        Me.dtpEndDate.Location = New System.Drawing.Point(118, 95)
+        Me.dtpEndDate.Name = "dtpEndDate"
+        Me.dtpEndDate.Size = New System.Drawing.Size(195, 20)
+        Me.dtpEndDate.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(29, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "End date:"
+        '
+        'dtpStartDate
+        '
+        Me.dtpStartDate.Location = New System.Drawing.Point(118, 63)
+        Me.dtpStartDate.Name = "dtpStartDate"
+        Me.dtpStartDate.Size = New System.Drawing.Size(195, 20)
+        Me.dtpStartDate.TabIndex = 3
+        '
+        'cmbDate
+        '
+        Me.cmbDate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbDate.FormattingEnabled = True
+        Me.cmbDate.Items.AddRange(New Object() {"Select Period", "Today", "This Month", "Last Month", "This Quarter", "Last Quarter", "This Year", "Last Year"})
+        Me.cmbDate.Location = New System.Drawing.Point(118, 30)
+        Me.cmbDate.Name = "cmbDate"
+        Me.cmbDate.Size = New System.Drawing.Size(195, 21)
+        Me.cmbDate.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Start date:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Select Period:"
+        '
+        'btnView
+        '
+        Me.btnView.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnView.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.btnView.FlatAppearance.BorderSize = 0
+        Me.btnView.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnView.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnView.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnView.Location = New System.Drawing.Point(124, 211)
+        Me.btnView.Name = "btnView"
+        Me.btnView.Size = New System.Drawing.Size(115, 34)
+        Me.btnView.TabIndex = 46
+        Me.btnView.Text = "View"
+        Me.btnView.UseVisualStyleBackColor = False
+        '
+        'btn_Cancel
+        '
+        Me.btn_Cancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_Cancel.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.btn_Cancel.FlatAppearance.BorderSize = 0
+        Me.btn_Cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Cancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Cancel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btn_Cancel.Location = New System.Drawing.Point(245, 211)
+        Me.btn_Cancel.Name = "btn_Cancel"
+        Me.btn_Cancel.Size = New System.Drawing.Size(115, 34)
+        Me.btn_Cancel.TabIndex = 45
+        Me.btn_Cancel.Text = "Cancel"
+        Me.btn_Cancel.UseVisualStyleBackColor = False
+        '
+        'lblview
+        '
+        Me.lblview.AutoSize = True
+        Me.lblview.Location = New System.Drawing.Point(22, 8)
+        Me.lblview.Name = "lblview"
+        Me.lblview.Size = New System.Drawing.Size(0, 13)
+        Me.lblview.TabIndex = 48
+        Me.lblview.Visible = False
+        '
+        'selectPeriod
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(378, 257)
+        Me.Controls.Add(Me.lblview)
+        Me.Controls.Add(Me.btnView)
+        Me.Controls.Add(Me.btn_Cancel)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "selectPeriod"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Select Period for the Report"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnView As System.Windows.Forms.Button
+    Friend WithEvents btn_Cancel As System.Windows.Forms.Button
+    Friend WithEvents dtpStartDate As System.Windows.Forms.DateTimePicker
+    Friend WithEvents cmbDate As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents dtpEndDate As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblview As System.Windows.Forms.Label
+End Class
---
+++ EuroAccounting/Forms/selectPeriod.vb
@@ -0,0 +1,62 @@
+﻿Public Class selectPeriod
+
+    Private Sub btnView_Click(sender As Object, e As EventArgs) Handles btnView.Click
+        Dim title As String = lblview.Text
+        Select Case title
+            Case "Trial Balance"
+                showUSC(uscTrialBalance)
+                Me.Close()
+            Case "Income Statement"
+                showUSC(uscIncomeStatement)
+                Me.Close()
+            Case "Cash Flow"
+                Me.Close()
+
+        End Select
+    End Sub
+
+    Private Sub btn_Cancel_Click(sender As Object, e As EventArgs) Handles btn_Cancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub selectDate_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbDate.SelectedIndex = 2
+    End Sub
+    Public Function LastDayOfMonth(ByVal sourceDate As DateTime) As DateTime
+        Dim lastDay As DateTime = New DateTime(sourceDate.Year, sourceDate.Month, 1)
+        Return lastDay.AddMonths(1).AddDays(-1)
+    End Function
+
+    Private Sub cmbDate_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbDate.SelectedIndexChanged
+        Dim selectedIndex As Integer
+        selectedIndex = cmbDate.SelectedIndex
+        If selectedIndex = 1 Then '#####DATE TODAY
+            dtpStartDate.Value = DateTime.Now
+            dtpEndDate.Value = DateTime.Now
+
+        ElseIf selectedIndex = 2 Then '#####THIS MONTH
+            Dim firstDay As DateTime = New DateTime(Now.Year, Now.Month, 1)
+            dtpStartDate.Value = firstDay
+            Dim lastDay As DateTime = New DateTime(Now.Year, Now.Month, 1).AddMonths(1).AddDays(-1)
+            dtpEndDate.Value = lastDay
+
+        ElseIf selectedIndex = 3 Then '#####LAST MONTH
+            Dim firstDay As DateTime = New DateTime(Now.Year, Now.Month, 1).AddMonths(-1)
+            dtpStartDate.Value = firstDay
+            Dim lastDay As DateTime = firstDay.AddMonths(1).AddDays(-1)
+            dtpEndDate.Value = lastDay
+
+        ElseIf selectedIndex = 4 Then '#####THIS QUARTER
+
+
+        ElseIf selectedIndex = 5 Then '#####LAST QUARTER
+
+
+        ElseIf selectedIndex = 6 Then '#####THIS YEAR
+
+
+        ElseIf selectedIndex = 7 Then '#####LAST YEAR
+
+        End If
+    End Sub
+End Class
