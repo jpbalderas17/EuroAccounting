72fea7a2423d12ac7ebe327cbf766b679c979b37
sgtsi-jenny <jenny.bercasio@sparkglobaltech.com>
2015-12-04 17:33:33 +0800

    Balance Sheet

--- EuroAccounting/UserControl/Balance_Sheet.vb
+++ EuroAccounting/UserControl/Balance_Sheet.vb
@@ -1,7 +1,8 @@
 ﻿
 Public Class Balance_Sheet
     Private Sub btn_Cancel_Click(sender As Object, e As EventArgs) Handles btn_Cancel.Click
-        showUSC(uscViewJournal)
-        uscViewJournal.cmbPost.Text = ""
+        'showUSC(uscViewJournal)
+        'uscViewJournal.cmbPost.Text = ""
+        showUSC(uscMainMenu)
     End Sub
 End Class
