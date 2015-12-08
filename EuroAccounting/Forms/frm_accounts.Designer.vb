15e7072ab2c5165bfcfca2c6303253eb93553465
eom-eom <elbertmolina_25@hotmail.com>
2015-12-08 12:56:35 +0800

    frm accounts

---
+++ EuroAccounting/Forms/frm_accounts.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_accounts
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_save = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btn_close = New System.Windows.Forms.Button()
+        Me.cbo_account_type = New System.Windows.Forms.ComboBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_account_name = New System.Windows.Forms.TextBox()
+        Me.lbl_account_id = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn_save
+        '
+        Me.btn_save.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_save.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_save.FlatAppearance.BorderSize = 0
+        Me.btn_save.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_save.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_save.ForeColor = System.Drawing.Color.Gold
+        Me.btn_save.Location = New System.Drawing.Point(272, 27)
+        Me.btn_save.Name = "btn_save"
+        Me.btn_save.Size = New System.Drawing.Size(93, 34)
+        Me.btn_save.TabIndex = 212
+        Me.btn_save.Text = "Save"
+        Me.btn_save.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label4.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Gold
+        Me.Label4.Location = New System.Drawing.Point(-2, 27)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(620, 34)
+        Me.Label4.TabIndex = 214
+        Me.Label4.Text = "    Accounts"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'btn_close
+        '
+        Me.btn_close.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_close.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_close.FlatAppearance.BorderSize = 0
+        Me.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_close.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_close.ForeColor = System.Drawing.Color.Gold
+        Me.btn_close.Location = New System.Drawing.Point(371, 27)
+        Me.btn_close.Name = "btn_close"
+        Me.btn_close.Size = New System.Drawing.Size(93, 34)
+        Me.btn_close.TabIndex = 215
+        Me.btn_close.Text = "Close"
+        Me.btn_close.UseVisualStyleBackColor = False
+        '
+        'cbo_account_type
+        '
+        Me.cbo_account_type.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbo_account_type.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbo_account_type.FormattingEnabled = True
+        Me.cbo_account_type.Items.AddRange(New Object() {"Super Administrator", "Administrator"})
+        Me.cbo_account_type.Location = New System.Drawing.Point(152, 117)
+        Me.cbo_account_type.Name = "cbo_account_type"
+        Me.cbo_account_type.Size = New System.Drawing.Size(268, 29)
+        Me.cbo_account_type.TabIndex = 217
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(12, 122)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(102, 21)
+        Me.Label6.TabIndex = 219
+        Me.Label6.Text = "Account Type"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 90)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 21)
+        Me.Label1.TabIndex = 218
+        Me.Label1.Text = "Account name"
+        '
+        'txt_account_name
+        '
+        Me.txt_account_name.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_account_name.Location = New System.Drawing.Point(152, 84)
+        Me.txt_account_name.Name = "txt_account_name"
+        Me.txt_account_name.Size = New System.Drawing.Size(268, 29)
+        Me.txt_account_name.TabIndex = 216
+        '
+        'lbl_account_id
+        '
+        Me.lbl_account_id.AutoSize = True
+        Me.lbl_account_id.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_account_id.Location = New System.Drawing.Point(104, 69)
+        Me.lbl_account_id.Name = "lbl_account_id"
+        Me.lbl_account_id.Size = New System.Drawing.Size(0, 21)
+        Me.lbl_account_id.TabIndex = 220
+        Me.lbl_account_id.Visible = False
+        '
+        'frm_accounts
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(476, 168)
+        Me.Controls.Add(Me.lbl_account_id)
+        Me.Controls.Add(Me.cbo_account_type)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_account_name)
+        Me.Controls.Add(Me.btn_close)
+        Me.Controls.Add(Me.btn_save)
+        Me.Controls.Add(Me.Label4)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frm_accounts"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Accounts"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btn_save As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btn_close As System.Windows.Forms.Button
+    Friend WithEvents cbo_account_type As System.Windows.Forms.ComboBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txt_account_name As System.Windows.Forms.TextBox
+    Friend WithEvents lbl_account_id As System.Windows.Forms.Label
+End Class
---
+++ EuroAccounting/Forms/frm_accounts.vb
@@ -0,0 +1,115 @@
+﻿Imports System.Data.SqlClient
+Public Class frm_accounts
+    Dim db As New DBHelper(My.Settings.connectionString)
+    Dim dr As SqlClient.SqlDataReader
+    Dim cmd As SqlClient.SqlCommand
+    Private Sub reset_form()
+        lbl_account_id.Text = ""
+        txt_account_name.Text = ""
+    End Sub
+    Private Sub frm_accounts_Load(sender As Object, e As EventArgs) Handles Me.Load
+        cbo_account_type_load()
+        reset_form()
+        If (uscAccounts.lvw_accounts.SelectedItems.Count = 1) Then
+            With uscAccounts.lvw_accounts.SelectedItems.Item(0)
+                Edit_this(CInt(.Text), CStr(.SubItems(1).Text), CStr(.SubItems(2).Text))
+            End With
+        End If
+    End Sub
+
+
+    Public Overloads Sub Show(id As Integer, name As String, type As String)
+        Me.Show()
+        Edit_this(id, name, type)
+        MsgBox(Me.Owner)
+    End Sub
+
+    Private Sub cbo_account_type_load()
+        Dim account_types As New ArrayList
+
+        dr = db.ExecuteReader("SELECT id,name FROM account_types")
+        Do While dr.Read
+            account_types.Add(New MyCombo(dr.Item("id"), dr.Item("name")))
+        Loop
+
+        With cbo_account_type
+            .DataSource = account_types 'Set arraylist as data source of combobox
+            .DisplayMember = "Description" 'as per class property
+            .ValueMember = "ID" 'as per class property
+        End With
+    End Sub
+    Private Sub save_account_type()
+        Dim input As New Dictionary(Of String, Object)
+        input.Add("name", txt_account_name.Text)
+        input.Add("type", cbo_account_type.SelectedValue)
+        Try
+
+
+            If (lbl_account_id.Text = "") Then
+                '## INSERT
+                If db.ExecuteNonQuery("INSERT INTO accounts(name,type) VALUES(@name,@type)", input) > 0 Then
+                    MsgBox("New Account Created", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "New Account")
+                    Me.Dispose()
+                Else
+                    MsgBox("Save Failed. Please try again", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Save Failed")
+
+                End If
+
+            Else
+                '##UPDATE
+                input.Add("id", lbl_account_id.Text)
+                If db.ExecuteNonQuery("UPDATE accounts SET name=@name,type=@type WHERE id=@id", input) > 0 Then
+                    MsgBox("Account Updated", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Update Account")
+                    Me.Dispose()
+                Else
+                    MsgBox("Save Failed. Please try again", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Save Failed")
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            db.Dispose()
+        End Try
+    End Sub
+
+    Private Sub btn_close_Click(sender As Object, e As EventArgs) Handles btn_close.Click
+        Me.Close()
+    End Sub
+    Private Function validate_account()
+        Dim is_valid As Boolean = True
+        Dim account_name, account_type, error_message As String
+
+        error_message = "Please fill up the following:" & vbCrLf
+        account_name = txt_account_name.Text
+        account_type = cbo_account_type.Text
+
+        If account_name = "" Then
+            error_message &= "Account Name" & vbCrLf
+            is_valid = False
+        End If
+        If account_type = "" Then
+            error_message &= "Account Type" & vbCrLf
+            is_valid = False
+        End If
+
+        If is_valid = False Then
+            MsgBox(error_message, MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "Invalid Data")
+        End If
+
+        Return is_valid
+    End Function
+    Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
+        If validate_account() = True Then
+            save_account_type()
+        End If
+    End Sub
+
+    Public Sub Edit_this(ByVal account_id As Integer, account_name As String, account_type As String)
+        With cbo_account_type
+            .SelectedIndex = .FindStringExact(account_type)
+        End With
+
+        lbl_account_id.Text = CInt(account_id)
+        txt_account_name.Text = CStr(account_name)
+    End Sub
+End Class
