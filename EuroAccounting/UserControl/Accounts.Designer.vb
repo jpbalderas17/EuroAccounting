6d98741018a298168974a478b652ff12f7f6caa7
eom-eom <elbertmolina_25@hotmail.com>
2015-12-08 13:41:19 +0800

    accounts

---
+++ EuroAccounting/UserControl/Accounts.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Accounts
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btn_Close = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lvw_accounts = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnDelete.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btnDelete.FlatAppearance.BorderSize = 0
+        Me.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.ForeColor = System.Drawing.Color.Gold
+        Me.btnDelete.Location = New System.Drawing.Point(817, 56)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(93, 34)
+        Me.btnDelete.TabIndex = 212
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAdd.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btnAdd.FlatAppearance.BorderSize = 0
+        Me.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.ForeColor = System.Drawing.Color.Gold
+        Me.btnAdd.Location = New System.Drawing.Point(619, 56)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(93, 34)
+        Me.btnAdd.TabIndex = 213
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnEdit.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btnEdit.FlatAppearance.BorderSize = 0
+        Me.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnEdit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEdit.ForeColor = System.Drawing.Color.Gold
+        Me.btnEdit.Location = New System.Drawing.Point(718, 56)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(93, 34)
+        Me.btnEdit.TabIndex = 211
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = False
+        '
+        'btn_Close
+        '
+        Me.btn_Close.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_Close.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_Close.FlatAppearance.BorderSize = 0
+        Me.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Close.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Close.ForeColor = System.Drawing.Color.Gold
+        Me.btn_Close.Location = New System.Drawing.Point(916, 56)
+        Me.btn_Close.Name = "btn_Close"
+        Me.btn_Close.Size = New System.Drawing.Size(93, 34)
+        Me.btn_Close.TabIndex = 210
+        Me.btn_Close.Text = "Close"
+        Me.btn_Close.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label4.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Gold
+        Me.Label4.Location = New System.Drawing.Point(-5, 56)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(1029, 34)
+        Me.Label4.TabIndex = 214
+        Me.Label4.Text = "    Accounts"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.lvw_accounts)
+        Me.Panel1.Location = New System.Drawing.Point(3, 96)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1018, 443)
+        Me.Panel1.TabIndex = 215
+        '
+        'lvw_accounts
+        '
+        Me.lvw_accounts.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lvw_accounts.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.lvw_accounts.Font = New System.Drawing.Font("Segoe UI Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lvw_accounts.FullRowSelect = True
+        Me.lvw_accounts.GridLines = True
+        Me.lvw_accounts.Location = New System.Drawing.Point(9, 14)
+        Me.lvw_accounts.Name = "lvw_accounts"
+        Me.lvw_accounts.Size = New System.Drawing.Size(997, 414)
+        Me.lvw_accounts.TabIndex = 8
+        Me.lvw_accounts.UseCompatibleStateImageBehavior = False
+        Me.lvw_accounts.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Account ID"
+        Me.ColumnHeader1.Width = 116
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Name"
+        Me.ColumnHeader2.Width = 397
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Type"
+        Me.ColumnHeader3.Width = 350
+        '
+        'Accounts
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.btn_Close)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Accounts"
+        Me.Size = New System.Drawing.Size(1024, 542)
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnEdit As System.Windows.Forms.Button
+    Friend WithEvents btn_Close As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents lvw_accounts As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ EuroAccounting/UserControl/Accounts.vb
@@ -0,0 +1,68 @@
+﻿Public Class Accounts
+    Dim db As New DBHelper(My.Settings.connectionString)
+    Dim dr As SqlClient.SqlDataReader
+    Dim cmd As SqlClient.SqlCommand
+
+    Private Sub btn_Close_Click(sender As Object, e As EventArgs) Handles btn_Close.Click
+        Me.Dispose()
+        showUSC(uscMainMenu)
+    End Sub
+    Private Sub lvw_accounts_load()
+        dr = db.ExecuteReader("SELECT a.id,a.name,at.name as type FROM accounts a JOIN account_types at ON a.type=at.id WHERE a.is_deleted=0")
+        If dr.HasRows Then
+            Do While dr.Read
+                With lvw_accounts.Items.Add(dr.Item("id").ToString)
+                    .SubItems.Add(dr.Item("name"))
+                    .SubItems.Add(dr.Item("type"))
+                End With
+            Loop
+        End If
+    End Sub
+
+    Private Sub Accounts_Load(sender As Object, e As EventArgs) Handles Me.Load
+        lvw_accounts_load()
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        lvw_accounts.SelectedItems.Clear()
+        Me.Enabled = False
+        frm_accounts.ShowDialog()
+        Me.Enabled = True
+        lvw_accounts_load()
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        If lvw_accounts.SelectedItems.Count = 1 Then
+            '## EDIT
+            With lvw_accounts.SelectedItems.Item(0)
+                Me.Enabled = False
+                frm_accounts.ShowDialog()
+                Me.Enabled = True
+                lvw_accounts_load()
+
+            End With
+        ElseIf lvw_accounts.SelectedItems.Count < 1 Then
+            MsgBox("Please select an account to edit.", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "No Account Selected")
+        ElseIf lvw_accounts.SelectedItems.Count > 1 Then
+            MsgBox("Multiple accounts selected. You can only edit one account at a time", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Multiple Accounts Selected")
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim account_id As Integer
+
+        If lvw_accounts.SelectedItems.Count = 1 Then
+            '## Delete
+            account_id = lvw_accounts.SelectedItems.Item(0).Text
+            If MsgBox("Delete this account?", MsgBoxStyle.Information + MsgBoxStyle.OkCancel, "Delete Account") = MsgBoxResult.Ok Then
+                db.ExecuteNonQuery("UPDATE accounts SET is_deleted=1 WHERE id=" & account_id)
+                lvw_accounts_load()
+            End If
+
+        ElseIf lvw_accounts.SelectedItems.Count < 1 Then
+            MsgBox("Please select an account to Delete.", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "No Account Selected")
+        ElseIf lvw_accounts.SelectedItems.Count > 1 Then
+            MsgBox("Multiple accounts selected. You can only delete one account at a time", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Multiple Accounts Selected")
+        End If
+    End Sub
+End Class
