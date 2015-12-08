70383a825d773d0d3c0cceb8831fc5a7474be8d1
sgtsi-jenny <jenny.bercasio@sparkglobaltech.com>
2015-12-08 14:20:15 +0800

    new USC Ledger

---
+++ EuroAccounting/UserControl/Ledgers.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Ledgers
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lvljournal = New System.Windows.Forms.ListView()
+        Me.chID = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chName = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chDesc = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.btnView = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btn_Close = New System.Windows.Forms.Button()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.lvljournal)
+        Me.Panel1.Controls.Add(Me.btnView)
+        Me.Panel1.Controls.Add(Me.btnDelete)
+        Me.Panel1.Controls.Add(Me.btnAdd)
+        Me.Panel1.Controls.Add(Me.btnEdit)
+        Me.Panel1.Controls.Add(Me.btn_Close)
+        Me.Panel1.Controls.Add(Me.lblDate)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(998, 558)
+        Me.Panel1.TabIndex = 20
+        '
+        'lvljournal
+        '
+        Me.lvljournal.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lvljournal.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lvljournal.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.chID, Me.chName, Me.chDesc})
+        Me.lvljournal.Font = New System.Drawing.Font("Segoe UI Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lvljournal.FullRowSelect = True
+        Me.lvljournal.GridLines = True
+        Me.lvljournal.Location = New System.Drawing.Point(5, 148)
+        Me.lvljournal.Name = "lvljournal"
+        Me.lvljournal.Size = New System.Drawing.Size(990, 298)
+        Me.lvljournal.TabIndex = 233
+        Me.lvljournal.UseCompatibleStateImageBehavior = False
+        Me.lvljournal.View = System.Windows.Forms.View.Details
+        '
+        'chID
+        '
+        Me.chID.Text = "ID"
+        Me.chID.Width = 147
+        '
+        'chName
+        '
+        Me.chName.Text = "Name"
+        Me.chName.Width = 468
+        '
+        'chDesc
+        '
+        Me.chDesc.Text = "Description"
+        Me.chDesc.Width = 505
+        '
+        'btnView
+        '
+        Me.btnView.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnView.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btnView.FlatAppearance.BorderSize = 0
+        Me.btnView.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnView.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnView.ForeColor = System.Drawing.Color.Gold
+        Me.btnView.Location = New System.Drawing.Point(506, 58)
+        Me.btnView.Name = "btnView"
+        Me.btnView.Size = New System.Drawing.Size(93, 34)
+        Me.btnView.TabIndex = 232
+        Me.btnView.Text = "View Journal"
+        Me.btnView.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnDelete.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btnDelete.FlatAppearance.BorderSize = 0
+        Me.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.ForeColor = System.Drawing.Color.Gold
+        Me.btnDelete.Location = New System.Drawing.Point(803, 58)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(93, 34)
+        Me.btnDelete.TabIndex = 230
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAdd.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btnAdd.FlatAppearance.BorderSize = 0
+        Me.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.ForeColor = System.Drawing.Color.Gold
+        Me.btnAdd.Location = New System.Drawing.Point(605, 58)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(93, 34)
+        Me.btnAdd.TabIndex = 231
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnEdit.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btnEdit.FlatAppearance.BorderSize = 0
+        Me.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnEdit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEdit.ForeColor = System.Drawing.Color.Gold
+        Me.btnEdit.Location = New System.Drawing.Point(704, 58)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(93, 34)
+        Me.btnEdit.TabIndex = 229
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = False
+        '
+        'btn_Close
+        '
+        Me.btn_Close.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_Close.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_Close.FlatAppearance.BorderSize = 0
+        Me.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Close.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Close.ForeColor = System.Drawing.Color.Gold
+        Me.btn_Close.Location = New System.Drawing.Point(902, 58)
+        Me.btn_Close.Name = "btn_Close"
+        Me.btn_Close.Size = New System.Drawing.Size(93, 34)
+        Me.btn_Close.TabIndex = 228
+        Me.btn_Close.Text = "Close"
+        Me.btn_Close.UseVisualStyleBackColor = False
+        '
+        'lblDate
+        '
+        Me.lblDate.AutoSize = True
+        Me.lblDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDate.Location = New System.Drawing.Point(81, 102)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(79, 20)
+        Me.lblDate.TabIndex = 225
+        Me.lblDate.Text = "Date Now"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(27, 102)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 20)
+        Me.Label1.TabIndex = 224
+        Me.Label1.Text = "Date:"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Gold
+        Me.Label2.Location = New System.Drawing.Point(0, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(995, 34)
+        Me.Label2.TabIndex = 42
+        Me.Label2.Text = "    Ledger"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Ledgers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Ledgers"
+        Me.Size = New System.Drawing.Size(1001, 558)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblDate As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnEdit As System.Windows.Forms.Button
+    Friend WithEvents btn_Close As System.Windows.Forms.Button
+    Friend WithEvents btnView As System.Windows.Forms.Button
+    Friend WithEvents lvljournal As System.Windows.Forms.ListView
+    Friend WithEvents chID As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chName As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chDesc As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ EuroAccounting/UserControl/Ledgers.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Ledgers
+    Dim db As New DBHelper(My.Settings.connectionString)
+    Dim dr As SqlClient.SqlDataReader
+    Dim cmd As SqlClient.SqlCommand
+    Private Sub btn_Cancel_Click(sender As Object, e As EventArgs)
+        'showUSC(uscViewJournal)
+        'uscViewJournal.cmbPost.Text = ""
+        showUSC(uscMainMenu)
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Ledger_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblDate.Text = Date.Now.ToString("MM/dd/yyyy")
+    End Sub
+
+    Private Sub cmbjournal_SelectedIndexChanged(sender As Object, e As EventArgs)
+        showUSC(uscViewJournal)
+    End Sub
+
+    Private Sub Ledgers_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lvLoadListview()
+    End Sub
+    Public Sub lvLoadListview()
+        Dim parameters As New Dictionary(Of String, Object)
+        lvljournal.Items.Clear()
+        Dim Item As ListViewItem
+        'parameters.Add("keyword", "%" & txtSearch.Text & "%")
+
+        Try
+            dr = db.ExecuteReader("SELECT id, name, description FROM ledgers", parameters)
+            If dr.HasRows Then
+                Do While dr.Read
+                    'Dim utype = (CInt(dr.Item("user_type")))
+                    Item = lvljournal.Items.Add(dr.Item("id").ToString)
+                    With Item
+                        .SubItems.Add(dr.Item("name"))
+                        .SubItems.Add(dr.Item("description"))
+                    End With
+                Loop
+            Else
+                MsgBox("No record of journals", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "No journals.")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        Finally
+            db.Dispose()
+        End Try
+    End Sub
+    Private Sub btn_Close_Click(sender As Object, e As EventArgs) Handles btn_Close.Click
+        showUSC(uscMainMenu)
+    End Sub
+    Private Sub viewJournal()
+        If lvljournal.SelectedItems.Count > 0 Then
+            Dim id As Integer = lvljournal.FocusedItem.Text
+            Try
+                dr = db.ExecuteReader("select * from ledgers WHERE id=" & lvljournal.FocusedItem.Text)
+                If dr.HasRows Then
+                    showUSC(uscViewJournal)
+
+                    'Dim pass = lvlUsers.SelectedItems(0).SubItems(4).Text
+                    'Dim decryptPass = Decrypt(pass, "Keys")
+                    'txtpwd.Text = decryptPass
+                    'txtconfirmpwd.Text = decryptPass
+
+                    'txtfullname.Text = lvlUsers.SelectedItems(0).SubItems(1).Text
+                    'txtuname.Text = lvlUsers.SelectedItems(0).SubItems(2).Text
+                    'cmbUtype.Text = lvlUsers.SelectedItems(0).SubItems(3).Text
+                    'txtpwd.Text = lvlUsers.SelectedItems(0).SubItems(4).Text
+                    'txtconfirmpwd.Text = lvlUsers.SelectedItems(0).SubItems(4).Text
+                End If
+
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            Finally
+                db.Dispose() '<--------CHECK THIS!
+            End Try
+        Else
+            MessageBox.Show("Please select ledger to view journals.", "Important Note", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1)
+        End If
+    End Sub
+    Private Sub btnView_Click(sender As Object, e As EventArgs) Handles btnView.Click
+        viewJournal()
+    End Sub
+End Class
