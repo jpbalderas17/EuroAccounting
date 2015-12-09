21bb6dd1867d87240c3d14ed564450467f17e034
sgtsi-jenny <jenny.bercasio@sparkglobaltech.com>
2015-12-09 11:32:17 +0800

    new form select_ledger

---
+++ EuroAccounting/Forms/select_ledger.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class select_ledger
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cbo_ledger = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn_close = New System.Windows.Forms.Button()
+        Me.btn_select = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cbo_ledger
+        '
+        Me.cbo_ledger.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbo_ledger.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbo_ledger.FormattingEnabled = True
+        Me.cbo_ledger.Location = New System.Drawing.Point(160, 75)
+        Me.cbo_ledger.Name = "cbo_ledger"
+        Me.cbo_ledger.Size = New System.Drawing.Size(195, 28)
+        Me.cbo_ledger.TabIndex = 50
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(47, 78)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(112, 20)
+        Me.Label1.TabIndex = 49
+        Me.Label1.Text = "Select Ledger:"
+        '
+        'btn_close
+        '
+        Me.btn_close.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_close.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_close.FlatAppearance.BorderSize = 0
+        Me.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_close.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_close.ForeColor = System.Drawing.Color.Gold
+        Me.btn_close.Location = New System.Drawing.Point(324, 19)
+        Me.btn_close.Name = "btn_close"
+        Me.btn_close.Size = New System.Drawing.Size(93, 34)
+        Me.btn_close.TabIndex = 218
+        Me.btn_close.Text = "Close"
+        Me.btn_close.UseVisualStyleBackColor = False
+        '
+        'btn_select
+        '
+        Me.btn_select.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_select.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_select.FlatAppearance.BorderSize = 0
+        Me.btn_select.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_select.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_select.ForeColor = System.Drawing.Color.Gold
+        Me.btn_select.Location = New System.Drawing.Point(227, 19)
+        Me.btn_select.Name = "btn_select"
+        Me.btn_select.Size = New System.Drawing.Size(93, 34)
+        Me.btn_select.TabIndex = 216
+        Me.btn_select.Text = "Select"
+        Me.btn_select.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label4.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Gold
+        Me.Label4.Location = New System.Drawing.Point(2, 19)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(436, 34)
+        Me.Label4.TabIndex = 217
+        Me.Label4.Text = "    Select Ledger"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'select_ledger
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(422, 124)
+        Me.Controls.Add(Me.btn_close)
+        Me.Controls.Add(Me.btn_select)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cbo_ledger)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "select_ledger"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Select Ledger"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cbo_ledger As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btn_close As System.Windows.Forms.Button
+    Friend WithEvents btn_select As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+End Class
---
+++ EuroAccounting/Forms/select_ledger.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Data.SqlClient
+Public Class select_ledger
+    Dim db As New DBHelper(My.Settings.connectionString)
+    Dim dr As SqlClient.SqlDataReader
+    Dim cmd As SqlClient.SqlCommand
+    Dim next_control As UserControl
+    Private Sub load_cbo()
+        Dim ledgers As New ArrayList
+        dr = db.ExecuteReader("SELECT id,name FROM ledgers ORDER by ID DESC")
+        If dr.HasRows Then
+            Do While dr.Read
+                ledgers.Add(New MyCombo(dr.Item("id"), dr.Item("name")))
+            Loop
+            With cbo_ledger
+                .DataSource = ledgers
+                .DisplayMember = "Description"
+                .ValueMember = "ID"
+            End With
+        End If
+    End Sub
+    Private Sub select_ledger_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        load_cbo()
+    End Sub
+    Public Overloads Sub Show(nextControl As UserControl)
+        Me.Show()
+        next_control = nextControl
+    End Sub
+
+   
+    Private Sub btn_select_Click(sender As Object, e As EventArgs) Handles btn_select.Click
+
+        Me.Hide()
+        showUSC(Me.next_control)
+        Me.Close()
+
+    End Sub
+End Class
