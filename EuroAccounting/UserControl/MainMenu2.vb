486ce7a01daeb75b363e977378db0bc9fd6b5786
sgtsi-jenny <jenny.bercasio@sparkglobaltech.com>
2015-12-10 10:28:40 +0800

    Main menu update

--- EuroAccounting/UserControl/MainMenu2.vb
+++ EuroAccounting/UserControl/MainMenu2.vb
@@ -31,9 +31,14 @@
     'End Sub
 
     Private Sub btn_Ledger_Click(sender As Object, e As EventArgs) Handles btn_Ledger.Click
-        'showUSC(uscLedger)
-        selectDate.lblview.Text = "Ledger"
-        selectDate.Show()
+        ''showUSC(uscLedger)
+        'selectDate.lblview.Text = "Ledger"
+        'selectDate.Show()
+
+
+        '# Create new ledger to reset data
+        uscLedger = New Ledger
+        select_ledger.Show(uscLedger)
     End Sub
     Private Sub btn_Balance_Sheet_Click(sender As Object, e As EventArgs) Handles btn_Balance_Sheet.Click
         selectDate.lblview.Text = "Balance Sheet"
@@ -46,13 +51,15 @@
     End Sub
 
     Private Sub btn_Trial_Balance_Click(sender As Object, e As EventArgs) Handles btn_Trial_Balance.Click
-        selectPeriod.lblview.Text = "Trial Balance"
-        selectPeriod.Show()
+        'selectPeriod.lblview.Text = "Trial Balance"
+        'selectPeriod.Show()
+        select_ledger.Show(uscTrialBalance)
     End Sub
 
     Private Sub btn_Income_Statement_Click(sender As Object, e As EventArgs) Handles btn_Income_Statement.Click
-        selectPeriod.lblview.Text = "Income Statement"
-        selectPeriod.Show()
+        'selectPeriod.lblview.Text = "Income Statement"
+        'selectPeriod.Show()
+        select_ledger.Show(uscIncomeStatement)
     End Sub
 
     Private Sub btnExit_Click_1(sender As Object, e As EventArgs) Handles btnExit.Click
